82b16980f552b05e166cf603bbf595247d0c4328
Bijan Chokoufe Nejad <bijan.chokoufe@esailors.de>
2018-06-16 15:58:07 +0200

    Add modifications

--- model.py
+++ model.py
@@ -1,14 +1,21 @@
 from __future__ import print_function
 import numpy as np
-np.random.seed(1337)  # for reproducibility
 import csv
 import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
+import itertools as it
+from itertools import *
 from keras.models import *
 from keras.layers import *
+
+
+SEED = 1337  # for reproducibility
+np.random.seed(SEED)
+batch_size = 1000
+sample_size = 14668 * 2
 
 
 def load_image(folder, source_path):
@@ -19,49 +26,55 @@ def load_image(folder, source_path):
 
 
 def load_csv(folder):
-  lines = []
-  with open(folder + 'driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    lines = [l for l in reader]
-    lines = lines[1:] # first line was a header in test data
-    correction = 0.10
-    images = []
-    angles = []
-    for l in lines:
-      center_angle = float(l[3])
-      if abs(center_angle) > 0.001:     # ignore lazy straight driving
-        left_angle = center_angle + correction
-        right_angle = center_angle - correction
-        img_center = load_image(folder, l[0])
-        img_left = load_image(folder, l[1])
-        img_right = load_image(folder, l[2])
-        images.append(img_center)
-        images.append(img_left)
-        images.append(img_right)
-        angles.append(center_angle)
-        angles.append(left_angle)
-        angles.append(right_angle)
-  return np.array(images), np.array(angles)
+  while True:   # because Keras interface doesn't make sense
+    with open(folder + 'driving_log.csv') as csvfile:
+      reader = csv.reader(csvfile)
+      lines = [l for l in reader]
+      lines = lines[1:] # first line was a header in test data
+      correction = 0.10
+      for l in lines:
+        center_angle = float(l[3])
+        if abs(center_angle) > 0.001:     # ignore lazy straight driving
+          left_angle = center_angle + correction
+          right_angle = center_angle - correction
+          img_center = load_image(folder, l[0])
+          img_left = load_image(folder, l[1])
+          img_right = load_image(folder, l[2])
+          yield (img_center, center_angle)
+          yield (img_left, left_angle)
+          yield (img_right, right_angle)
+          yield (np.fliplr(img_center), - center_angle)
+          yield (np.fliplr(img_left), - left_angle)
+          yield (np.fliplr(img_right), - right_angle)
+
+
+def batch(generator, size=batch_size):
+  acc_x = []
+  acc_y = []
+  for x, y in generator:
+    acc_x.append(x)
+    acc_y.append(y)
+    if len(acc_x) == size:
+      yield np.array(acc_x), np.array(acc_y)
+      acc_x, acc_y = [], []
 
 
 def load_data():
-  X_train, Y_train = load_csv('data/')
-  X_dirt, Y_dirt = load_csv('dirtcorner/')
-  X_dirt_extra, Y_dirt_extra = load_csv('dirt_extra/')
-  X_extra, Y_extra = load_csv('more/')
-  X_train = np.append(X_train, X_dirt, axis=0)
-  Y_train = np.append(Y_train, Y_dirt, axis=0)
-  X_train = np.append(X_train, X_extra, axis=0)
-  Y_train = np.append(Y_train, Y_extra, axis=0)
-  X_train = np.append(X_train, X_dirt_extra, axis=0)
-  Y_train = np.append(Y_train, Y_dirt_extra, axis=0)
-  return X_train, Y_train
+  generator_train = load_csv('data/')
+  generator_dirt = load_csv('dirtcorner/')
+  generator_dirt_extra = load_csv('dirt_extra/')
+  generator_extra = load_csv('more/')
+  generator = chain(generator_train, generator_dirt,
+                    generator_dirt_extra, generator_extra)
+  return batch(generator)
 
 
 def setup_model():
+  fraction_to_drop = 0.25
   model = Sequential()
   model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
   model.add(Cropping2D(cropping=((70,25),(0,0))))
+  model.add(Dropout(fraction_to_drop, noise_shape=None, seed=SEED))
   # 5 conv nets
   model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
   model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
@@ -69,9 +82,10 @@ def setup_model():
   model.add(Convolution2D(64,3,3,activation="relu"))
   model.add(Convolution2D(64,3,3,activation="relu"))
   model.add(Flatten())
+  model.add(Dropout(fraction_to_drop, noise_shape=None, seed=SEED))
   # 4 fully connected
-  model.add(Dense(100))
-  model.add(Dense(50))
+  model.add(Dense(100,activation="relu"))
+  model.add(Dense(50,activation="relu"))
   model.add(Dense(10))
   model.add(Dense(1))
   model.compile(loss='mean_absolute_error', optimizer='adam')
@@ -85,12 +99,12 @@ def analyze_angles(Y_train):
 
 
 if __name__ == "__main__":
-  X_train, Y_train = load_data()
-  analyze_angles(Y_train)
+  generator = load_data()
+  #  analyze_angles(Y_train)
   model = setup_model()
-  model.fit(X_train, Y_train,
-            validation_split=0.2,
+  model.fit_generator(generator,
+            steps_per_epoch=sample_size/batch_size + 1,
+            epochs=30,
             shuffle=True,
-            epochs=3,
             verbose=1)
   model.save('model.h5')
